38c8bef958113645365fba4fdc9ebfa708202cb7
BL-Chandana <chandana.sree@bridgelabz.com>
2026-02-06 16:29:29 +0530

    [Chandana]-Feat:UC5-Game Result Storage

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,5 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GuessingApp {
+
+    public static void saveResult(String result) {
+        try {
+            FileWriter writer = new FileWriter("game_results.txt", true);
+            writer.write(result + "\n");
+            writer.close();
+            System.out.println("Game result saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error while saving game result.");
+        }
+    }
+
+    // main method to run in Eclipse
     public static void main(String[] args) {
-        
+        saveResult("Test Result");
     }
 }
