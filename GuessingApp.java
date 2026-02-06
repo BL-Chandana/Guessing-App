97e5b7844a78a8e02553316948e330673ebf8462
BL-Chandana <chandana.sree@bridgelabz.com>
2026-02-06 16:13:49 +0530

    [Chandana]-Feat:UC3-Hint Generation

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class GuessingApp {
+
+    // HintService logic inside same file
+    static class HintService {
+
+        public static void showHint(int targetNumber) {
+            if (targetNumber % 2 == 0) {
+                System.out.println("Hint: The number is EVEN");
+            } else {
+                System.out.println("Hint: The number is ODD");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        // Call hint method
+        HintService.showHint(number);
+
+        scanner.close();
     }
 }
