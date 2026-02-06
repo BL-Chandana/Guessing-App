694db66a35897cc5a9deebcf6ff6008c451d0d45
BL-Chandana <chandana.sree@bridgelabz.com>
2026-02-06 16:04:10 +0530

    [Chandana]-Feat:UC2-User Guess Submission Scanner input

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,5 +1,22 @@
 public class GuessingApp {
+
     public static void main(String[] args) {
-        
+
+        int guess = 10;
+        int target = 20;
+
+        String result = validateGuess(guess, target);
+        System.out.println(result);
+    }
+
+    public static String validateGuess(int guess, int target) {
+        if (guess == target) {
+            return "CORRECT";
+        } else if (guess > target) {
+            return "HIGH";
+        } else {
+            return "LOW";
+        }
     }
 }
+
