7934c93da1d89d9ae11df63faf0d385ff5287eb7
BL-Chandana <chandana.sree@bridgelabz.com>
2026-02-06 16:33:09 +0530

    [Chandana]-Feat:UC6-game restart or exit

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,5 +1,21 @@
-public class GuessingApp {
+import java.util.Scanner;
+
+public class GuessingApp{
+
+    public static boolean restartGame() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Do you want to play again? (yes/no): ");
+        String choice = scanner.next();
+
+        return choice.equalsIgnoreCase("yes");
+    }
+
+    // main method to run the code
     public static void main(String[] args) {
-        
+
+        boolean restart = restartGame();
+
+        System.out.println("Restart game: " + restart);
     }
 }
